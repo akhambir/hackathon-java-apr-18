4c4a28b48ab713e6ea747f8b69e0b4ec13f76d9c
ivaniuminov <ivan.iuminov@gmail.com>
2018-08-18 16:46:20 +0300

    Product

--- onlineshop-server/src/main/java/com/hackathon/controller/ProductController.java
+++ onlineshop-server/src/main/java/com/hackathon/controller/ProductController.java
@@ -1,7 +1,24 @@
 package com.hackathon.controller;
 
+import com.hackathon.model.Product;
+import com.hackathon.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @RequestMapping(path = "/product/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Product> getById(@PathVariable String id) {
+        return productService.getById(Long.valueOf(id))
+                .map(ResponseEntity::ok)
+                .orElseGet(ResponseEntity.notFound()::build);
+    }
 }
