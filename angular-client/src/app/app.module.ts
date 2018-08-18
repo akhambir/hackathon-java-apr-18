9e39889d9d1d7d547fbe68230e8d0edb2e918fa2
evgenknyzhenko <evgen.knyzhenko@gmail.com>
2018-08-18 12:43:36 +0300

    Login page

--- angular-client/src/app/app.module.ts
+++ angular-client/src/app/app.module.ts
@@ -2,15 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginUserComponent } from './login-user/login-user.component';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+import {UserService} from './services/user-service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginUserComponent
   ],
   imports: [
-    BrowserModule
+    HttpClientModule,
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
