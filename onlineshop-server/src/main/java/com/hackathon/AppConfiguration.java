235ee5be2ba4f62af52dece7c634857b0204e760
ivaniuminov <ivan.iuminov@gmail.com>
2018-08-18 11:52:22 +0300

    properties

--- onlineshop-server/src/main/java/com/hackathon/AppConfiguration.java
+++ onlineshop-server/src/main/java/com/hackathon/AppConfiguration.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AppConfiguration {
 
     public static void main(String[] args) {
-        SpringApplication.run(AppConfiguration.class);
+        SpringApplication.run(AppConfiguration.class, args);
     }
 }
