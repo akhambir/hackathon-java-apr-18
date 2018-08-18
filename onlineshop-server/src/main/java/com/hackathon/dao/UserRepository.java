5794c963fe5509e246003b1e3b9df4a353613314
ivaniuminov <ivan.iuminov@gmail.com>
2018-08-18 15:03:28 +0300

    Categories

--- onlineshop-server/src/main/java/com/hackathon/dao/UserRepository.java
+++ onlineshop-server/src/main/java/com/hackathon/dao/UserRepository.java
@@ -1,7 +1,12 @@
 package com.hackathon.dao;
 
+import com.hackathon.model.Product;
 import com.hackathon.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    List<Product> getAll();
 }
