39010d3ab8782fefc230ba6bb71614f9dfe787ba
ivaniuminov <ivan.iuminov@gmail.com>
2018-08-19 11:54:17 +0300

    DB

--- onlineshop-server/src/main/java/com/hackathon/dao/ProductRepository.java
+++ onlineshop-server/src/main/java/com/hackathon/dao/ProductRepository.java
@@ -4,6 +4,11 @@ import com.hackathon.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Optional<List<Product>> getAllByCategory_Id(Long id);
 }
